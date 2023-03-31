6754af32c39807bf8b038b7b9b8d21699dcb01db
notnic <nicovo989@gmail.com>
2023-03-31 18:26:39 +0100

    Created ServiceHelper.java for entity services to utilise

---
+++ src/main/java/uk/notnic/fsdashboard/service/ServiceHelper.java
@@ -0,0 +1,15 @@
+package uk.notnic.fsdashboard.service;
+
+import org.dom4j.DocumentException;
+
+public class ServiceHelper {
+
+    public ServiceHelper() {
+
+    }
+
+    public void createEntityFromXML(String filepath) throws DocumentException {
+
+    }
+
+}
