f5662ca69217c9d4466dcc135c286b3f3722fade
notnic <nicovo989@gmail.com>
2023-03-31 18:25:10 +0100

    Created Career.java Entity

---
+++ src/main/java/uk/notnic/fsdashboard/model/Career.java
@@ -0,0 +1,48 @@
+package uk.notnic.fsdashboard.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "career")
+@SequenceGenerator(name="career_seq", allocationSize=1)
+public class Career {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "career_seq")
+    private Long id;
+    private String saveGameName;
+    private String mapTitle;
+    private Integer difficulty;
+    private String dateCreated;
+
+    public Career() {
+    }
+
+    public Career(Long id, String saveGameName, String mapTitle, Integer difficulty, String dateCreated) {
+        this.id = id;
+        this.saveGameName = saveGameName;
+        this.mapTitle = mapTitle;
+        this.difficulty = difficulty;
+        this.dateCreated = dateCreated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSaveGameName() {
+        return saveGameName;
+    }
+
+    public String getMapTitle() {
+        return mapTitle;
+    }
+
+    public Integer getDifficulty() {
+        return difficulty;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+}
