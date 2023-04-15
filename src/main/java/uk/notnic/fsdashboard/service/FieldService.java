dfb97f4c5bb67b818857777e1c4a554e24fd2937
notnic <nicovo989@gmail.com>
2023-04-15 02:35:54 +0100

    Created FieldService.java which uses Unmarshaller to read files

---
+++ src/main/java/uk/notnic/fsdashboard/service/FieldService.java
@@ -0,0 +1,41 @@
+package uk.notnic.fsdashboard.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.notnic.fsdashboard.model.Fields.Field;
+import uk.notnic.fsdashboard.model.Fields.Fields;
+import uk.notnic.fsdashboard.repository.FieldRepository;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.util.List;
+
+@Service
+public class FieldService extends ServiceHelper {
+
+    private final FieldRepository fieldRepository;
+
+    @Autowired
+    public FieldService(FieldRepository fieldRepository) {
+        this.fieldRepository = fieldRepository;
+    }
+
+    public List<Field> getAllFields() {
+        return fieldRepository.findAll();
+    }
+
+    public void createEntityFromXML(String filepath) throws JAXBException {
+        File file = new File(filepath);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Fields.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        Fields fields = (Fields) jaxbUnmarshaller.unmarshal(file);
+
+        for (Field field : fields.getFields()) {
+            System.out.println("Planned Fruit: " + field.getPlannedFruit());
+            Field newField = new Field(field.getPlannedFruit());
+            fieldRepository.save(newField);
+        }
+    }
+}
