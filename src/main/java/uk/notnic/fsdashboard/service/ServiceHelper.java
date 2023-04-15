97f2fb44a55302fedd680461f30af7b4fe7679b9
notnic <nicovo989@gmail.com>
2023-04-15 02:37:53 +0100

    Added JAXBException to ServiceHelper.java

--- src/main/java/uk/notnic/fsdashboard/service/ServiceHelper.java
+++ src/main/java/uk/notnic/fsdashboard/service/ServiceHelper.java
@@ -2,13 +2,15 @@ package uk.notnic.fsdashboard.service;
 
 import org.dom4j.DocumentException;
 
+import javax.xml.bind.JAXBException;
+
 public class ServiceHelper {
 
     public ServiceHelper() {
 
     }
 
-    public void createEntityFromXML(String filepath) throws DocumentException {
+    public void createEntityFromXML(String filepath) throws DocumentException, JAXBException {
 
     }
 
