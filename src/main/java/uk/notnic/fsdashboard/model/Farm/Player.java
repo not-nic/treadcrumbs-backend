d884503c35f72a84bd4a8b18257a09b704ef4a2e
notnic <nicovo989@gmail.com>
2023-04-15 02:37:21 +0100

    Created Player.java and Players.java Entity & JAXB Classes.

---
+++ src/main/java/uk/notnic/fsdashboard/model/Farm/Player.java
@@ -0,0 +1,41 @@
+package uk.notnic.fsdashboard.model.Farm;
+
+
+import javax.persistence.Embeddable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Embeddable
+public class Player {
+
+    @XmlAttribute(name = "uniqueUserId")
+    private String uniqueUserId;
+    @XmlAttribute(name = "lastNickname")
+    private String playerName;
+
+    public Player() {
+    }
+
+    public Player(String uniqueUserId, String playerName) {
+        this.uniqueUserId = uniqueUserId;
+        this.playerName = playerName;
+    }
+
+    public String getUniqueUserId() {
+        return uniqueUserId;
+    }
+
+    public void setUniqueUserId(String uniqueUserId) {
+        this.uniqueUserId = uniqueUserId;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
---
+++ src/main/java/uk/notnic/fsdashboard/model/Farm/Players.java
@@ -0,0 +1,29 @@
+package uk.notnic.fsdashboard.model.Farm;
+
+import javax.persistence.Embeddable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Embeddable
+public class Players {
+
+    @XmlElement(name = "player")
+    private Player player;
+
+    public Players() {
+    }
+
+    public Players(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
