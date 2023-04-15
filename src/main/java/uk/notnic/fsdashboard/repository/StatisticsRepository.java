c1a981462e39cb002c33c4a9caf44ebb74a87be0
notnic <nicovo989@gmail.com>
2023-04-15 02:39:03 +0100

    Created StatisticsRepository.java

--- src/main/java/uk/notnic/fsdashboard/repository/StatisticsRepository.java
+++ src/main/java/uk/notnic/fsdashboard/repository/StatisticsRepository.java
@@ -1,12 +1,10 @@
 package uk.notnic.fsdashboard.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import uk.notnic.fsdashboard.model.Statistics;
+import uk.notnic.fsdashboard.model.Farm.Statistics;
 
 import java.util.List;
 
-@Repository
 public interface StatisticsRepository extends CrudRepository<Statistics, Long> {
     List<Statistics> findAll();
 }
