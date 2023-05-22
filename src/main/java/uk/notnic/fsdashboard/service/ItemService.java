c0aa2a0b1e07b5e7349fac56766cb64105ede952
notnic <nicovo989@gmail.com>
2023-05-22 21:25:39 +0100

    Created ItemService and organised bales into their different types i.e. Straw, Hay, Silage and Grass

---
+++ src/main/java/uk/notnic/fsdashboard/service/ItemService.java
@@ -0,0 +1,72 @@
+package uk.notnic.fsdashboard.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.notnic.fsdashboard.model.Coordinate;
+import uk.notnic.fsdashboard.model.Items.Bale;
+import uk.notnic.fsdashboard.model.Items.Item;
+import uk.notnic.fsdashboard.model.Items.Items;
+import uk.notnic.fsdashboard.repository.BaleRepository;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.util.List;
+
+@Service
+public class ItemService extends ServiceHelper {
+
+    private final BaleRepository baleRepository;
+
+    @Autowired
+    public ItemService(BaleRepository baleRepository) {
+        this.baleRepository = baleRepository;
+    }
+
+    public List<Bale> getAllBales() {
+        return baleRepository.findAll();
+    }
+
+    @Override
+    public void createEntityFromXML(String filepath) throws JAXBException {
+        File file = new File(filepath);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Items.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        Items items = (Items) jaxbUnmarshaller.unmarshal(file);
+
+        // total fill levels for all bale types
+        double totalStrawFillLevel = 0;
+        double totalHayFilLevel = 0;
+        double totalGrassFillLevel = 0;
+        double totalSilageFillLevel = 0;
+
+        // bale counters
+        int strawCounter = 0;
+        int hayCounter = 0;
+        int grassCounter = 0;
+        int silageCounter = 0;
+
+        for (Item item : items.getItems()) {
+            // change coordinate gathered from XML file from string to Coordinate class
+            item.setPosition(new Coordinate().createCoordinate(item.getPos()));
+
+            // check if the class name is of type Bale
+            if (item.getClassName().equals("Bale")) {
+
+                switch (item.getFillType()) {
+                    case "STRAW": totalStrawFillLevel += item.getFillLevel(); strawCounter++; break;
+                    case "DRYGRASS_WINDROW": totalHayFilLevel += item.getFillLevel(); hayCounter++; break;
+                    case "GRASS": totalGrassFillLevel += item.getFillLevel(); grassCounter++; break;
+                    case "SILAGE": totalSilageFillLevel += item.getFillLevel(); silageCounter++; break;
+                }
+            }
+        }
+
+        baleRepository.save(new Bale(null, strawCounter, "straw", totalStrawFillLevel));
+        baleRepository.save(new Bale(null, hayCounter, "hay", totalHayFilLevel));
+        baleRepository.save(new Bale(null, grassCounter, "grass", totalGrassFillLevel));
+        baleRepository.save(new Bale(null, silageCounter, "silage", totalSilageFillLevel));
+    }
+
+}
