33b3359ce137b79cb6ae32d6e4ec7bea59759437
notnic <nicovo989@gmail.com>
2023-05-22 21:25:01 +0100

    Created ItemController.java

---
+++ src/main/java/uk/notnic/fsdashboard/controller/ItemController.java
@@ -0,0 +1,30 @@
+package uk.notnic.fsdashboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.notnic.fsdashboard.model.Items.Bale;
+import uk.notnic.fsdashboard.service.ItemService;
+
+import javax.xml.bind.JAXBException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/test-items")
+    public void testItems() throws JAXBException {
+        itemService.createEntityFromXML("C:\\Users\\Nick\\IdeaProjects\\fsdashboard\\uploads\\savegame3/items.xml");
+    }
+    @GetMapping("/bales")
+    public List<Bale> getAllBales() {
+        return itemService.getAllBales();
+    }
+}
