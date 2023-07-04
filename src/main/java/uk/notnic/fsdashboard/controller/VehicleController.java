560917005fc13f95841cfe0e665a45a07bd42e64
notnic <nicovo989@gmail.com>
2023-07-04 22:15:54 +0100

    Created mappings for updating and Finding vehicles by id.

--- src/main/java/uk/notnic/fsdashboard/controller/VehicleController.java
+++ src/main/java/uk/notnic/fsdashboard/controller/VehicleController.java
@@ -8,6 +8,8 @@ import uk.notnic.fsdashboard.model.Vehicles.Vehicle;
 import uk.notnic.fsdashboard.service.VehicleService;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
 public class VehicleController {
@@ -23,9 +25,29 @@ public class VehicleController {
         vehicleService.createEntityFromXML("C:\\Users\\Nick\\IdeaProjects\\fsdashboard\\uploads\\project_savegame/vehicles.xml");
     }
 
+    @GetMapping("/implements/{id}")
+    public Optional<Implement> findImplementById(@PathVariable long id) {
+        return vehicleService.getImplementById(id);
+    }
+
+    @PostMapping("/implements/{id}")
+    public Implement updateImplement(@PathVariable long id, @RequestBody Implement implement) {
+        return vehicleService.updateImplement(id, implement);
+    }
+
     @GetMapping("/implements")
     public List<Implement> allImplements() {
         return vehicleService.getAllImplements();
+    }
+
+    @GetMapping("tractors/{id}")
+    public Optional<Tractor> findTractorById(@PathVariable long id) {
+        return vehicleService.getTractorById(id);
+    }
+
+    @PostMapping("tractors/{id}")
+    public Tractor updateTractor(@PathVariable long id, @RequestBody Tractor tractor) {
+        return vehicleService.updateTractor(id, tractor);
     }
 
     @GetMapping("tractors")
