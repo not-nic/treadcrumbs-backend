6e93b01eadf3c1077febeb16fc6e80c45bfd88be
notnic <nicovo989@gmail.com>
2023-07-19 11:05:23 +0100

    Remove id setter from field.java

--- src/main/java/uk/notnic/fsdashboard/model/Fields/Field.java
+++ src/main/java/uk/notnic/fsdashboard/model/Fields/Field.java
@@ -46,10 +46,6 @@ public class Field {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getPreviousCrop() {
         return previousCrop;
     }
