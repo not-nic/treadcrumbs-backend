bd2b0a54c10579f43317a7ce03e34b83a3bc79d7
notnic <nicovo989@gmail.com>
2023-07-23 23:45:40 +0100

    Created Find Farm by id repository query.

--- src/main/java/uk/notnic/fsdashboard/repository/FarmRepository.java
+++ src/main/java/uk/notnic/fsdashboard/repository/FarmRepository.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository;
 import uk.notnic.fsdashboard.model.Farms.Farm;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FarmRepository extends CrudRepository<Farm, Long> {
     List<Farm> findAll();
+
+    Optional<Farm> findById(long id);
 }
