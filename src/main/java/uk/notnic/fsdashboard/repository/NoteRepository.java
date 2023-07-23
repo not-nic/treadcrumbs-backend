ac9190e4bcacc4a9ba733a15ba5535298d1c72ec
notnic <nicovo989@gmail.com>
2023-07-23 18:03:38 +0100

    Created Note Repository.

---
+++ src/main/java/uk/notnic/fsdashboard/repository/NoteRepository.java
@@ -0,0 +1,13 @@
+package uk.notnic.fsdashboard.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import uk.notnic.fsdashboard.model.Notes.Note;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NoteRepository extends CrudRepository<Note, Long> {
+    List<Note> findAll();
+    Optional<Note> findById(Long id);
+    void deleteById(Long id);
+}
