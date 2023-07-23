684d1c74c489c5fbd558fcf6e497bce4e1668b67
notnic <nicovo989@gmail.com>
2023-07-23 23:45:59 +0100

    Create Finance Mapping.

--- src/main/java/uk/notnic/fsdashboard/controller/FinanceController.java
+++ src/main/java/uk/notnic/fsdashboard/controller/FinanceController.java
@@ -1,8 +1,6 @@
 package uk.notnic.fsdashboard.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.notnic.fsdashboard.model.Farms.FinanceStats;
 import uk.notnic.fsdashboard.service.FinanceService;
 
@@ -21,5 +19,12 @@ public class FinanceController {
     @GetMapping("/finances")
     public List<FinanceStats> getFinances() {
         return financeService.getAllFinances();
+    }
+
+    @PostMapping("/finances")
+    public String createFinanceStat(@RequestBody FinanceStats financeStats) throws IllegalAccessException {
+        financeService.createFinanceStat(financeStats);
+
+        return String.format("Finance Stat %s created.", financeStats.getId());
     }
 }
