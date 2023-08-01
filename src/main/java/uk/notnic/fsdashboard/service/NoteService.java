a343c9ce4346dfbed3c419e6a033c666e3188aae
notnic <nicovo989@gmail.com>
2023-08-01 01:31:37 +0100

    Created helper functions for extracting arguments from notes using  regex, and created function for seed notes.

--- src/main/java/uk/notnic/fsdashboard/service/NoteService.java
+++ src/main/java/uk/notnic/fsdashboard/service/NoteService.java
@@ -3,23 +3,33 @@ package uk.notnic.fsdashboard.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.notnic.fsdashboard.model.Fields.Field;
 import uk.notnic.fsdashboard.model.Notes.Note;
+import uk.notnic.fsdashboard.repository.FieldRepository;
 import uk.notnic.fsdashboard.repository.NoteRepository;
+import uk.notnic.fsdashboard.repository.TractorRepository;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class NoteService {
 
     private final NoteRepository noteRepository;
+    private final FieldRepository fieldRepository;
+    private final TractorRepository tractorRepository;
     ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
-    public NoteService(NoteRepository noteRepository) {
+    public NoteService(NoteRepository noteRepository, FieldRepository fieldRepository, TractorRepository tractorRepository) {
         this.noteRepository = noteRepository;
+        this.fieldRepository = fieldRepository;
+        this.tractorRepository = tractorRepository;
     }
 
     public List<Note> getAllNotes() {
@@ -32,6 +42,66 @@ public class NoteService {
 
     public void createNote(Note note) {
         note.setCreated(LocalDateTime.now());
+        noteRepository.save(note);
+    }
+
+    private List<String> findArguments(String noteContents) {
+        List<String> matches = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("\\b(\\S+:\\S+)");
+        Matcher matcher = pattern.matcher(noteContents);
+
+        while (matcher.find()) {
+            String match = matcher.group(1);
+            matches.add(match);
+        }
+
+        return matches;
+    }
+
+    private Object findNoteValues(String foundArgument) {
+        Pattern pattern = Pattern.compile(":\\s*([\\w\\d]+)");
+        Matcher matcher = pattern.matcher(foundArgument);
+
+        if (matcher.find()) {
+            String value = matcher.group(1);
+            try {
+                return Long.parseLong(value);
+            } catch (NumberFormatException e) {
+                return value;
+            }
+        }
+
+        return null;
+    }
+
+    public void createSeedCommand(Note note) {
+
+        note.setCreated(LocalDateTime.now());
+        StringBuilder newContents = new StringBuilder();
+
+        String contents = note.getContents();
+
+        List<String> arguments = findArguments(contents);
+
+        for (String argument : arguments) {
+            Object value = findNoteValues(argument);
+
+            if (value instanceof Long argumentValue) {
+                String argumentName = argument.split(":")[0];
+
+                if ("field".equals(argumentName)) {
+                    fieldRepository.findById(argumentValue).ifPresent(field -> newContents.append(String.format("/Seed field %s with ", field.getFarmlandId())));
+
+                } else if ("vehicle".equals(argumentName)) {
+                    tractorRepository.findById(argumentValue).ifPresent(tractor -> newContents.append(String.format(" using %s.", tractor.getName())));
+                }
+            } else if (value instanceof String cropName) {
+                newContents.append(String.format("[%s]", cropName));
+            }
+        }
+
+        note.setContents(newContents.toString());
         noteRepository.save(note);
     }
 
